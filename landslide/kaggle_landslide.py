f0af55fb842610bad037eb65880ff6db132bff91
Mystyk00 <yyt262960@gmail.com>
2024-11-09 22:13:19 +0200

    initial commit

---
+++ landslide/kaggle_landslide.py
@@ -0,0 +1,5 @@
+import kagglehub
+
+path = kagglehub.dataset_download("tekbahadurkshetri/landslide4sense")
+
+print("Path to dataset files:", path)
---
+++ wildfire/kaggle_wildfire.py
@@ -0,0 +1,3 @@
+import kagglehub
+path = kagglehub.dataset_download("abdelghaniaaba/wildfire-prediction-dataset")
+print("Path to dataset files:", path)
